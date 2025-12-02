c5fa949d1a1bbc5b9aa72d43b152b2e3db093da7
fir3e <fir3ez@gmail.com>
2025-12-02 21:59:10 +0100

    87. Aura Asset Manager

---
+++ Source/Aura/Private/AuraAssetManager.cpp
@@ -0,0 +1,20 @@
+// Copyright fir3e
+
+
+#include "AuraAssetManager.h"
+#include "AuraGameplayTags.h"
+
+UAuraAssetManager& UAuraAssetManager::Get()
+{
+	check(GEngine);
+	
+	UAuraAssetManager* AuraAssetManager = Cast<UAuraAssetManager>(GEngine->AssetManager);
+	return *AuraAssetManager;
+}
+
+void UAuraAssetManager::StartInitialLoading()
+{
+	Super::StartInitialLoading();
+	
+	FAuraGameplayTags::InitializeNativeGameplayTags();
+}
